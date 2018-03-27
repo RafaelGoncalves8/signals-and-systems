855dcdf70c972bf6afad9f2fd655c6453dc3b0ca
Rafael Goncalves - 186062 <ra186062@le22-18.grad.fee.unicamp.br>
2018-03-27 16:15:26 -0300

    begin exercise d

--- 2-frequency_domain/efc2.m
+++ 2-frequency_domain/efc2.m
@@ -1,42 +1,59 @@
 x = @(n,w) cos(n*w);
 N = 1000;
 
-% a)
-w = pi/2;
-X = [];
-for i = 0:N,
-    X(i+1) = x(i,w);
-end;
-Y = sistema_desconhecido(X)
-stem(X)
-hold on
-stem(Y, 'm')
-legend('X', 'Y')
-hold off
+% % a)
+% w = pi/2;
+% X = [];
+% for i = 0:N,
+%     X(i+1) = x(i,w);
+% end;
+% Y = sistema_desconhecido(X)
+% stem(X)
+% hold on
+% stem(Y, 'm')
+% legend('X', 'Y')
+% hold off
 
 
 % b)
-w = pi/200;
-X = [];
-for i = 0:N,
-    X(i+1) = x(i,w);
-end;
-Y = sistema_desconhecido(X)
-stem(Y)
-hold on
+% w = pi/200;
+% X = [];
+% for i = 0:N,
+%     X(i+1) = x(i,w);
+% end;
+% Y = sistema_desconhecido(X)
+% stem(Y)
+% hold on
+% 
+% w = pi/20;
+% X = [];
+% for i = 0:N,
+%     X(i+1) = x(i,w);
+% end;
+% Y = sistema_desconhecido(X)
+% stem(Y, 'm')
+% legend('w = pi/200', 'w = pi/20')
+% hold off
 
-w = pi/20;
-X = [];
-for i = 0:N,
-    X(i+1) = x(i,w);
-end;
-Y = sistema_desconhecido(X)
-stem(Y, 'm')
-legend('w = pi/200', 'w = pi/20')
-hold off
+% c)
 
-% % c)
-% 
-% % d)
-% 
-% 
+scale = @(Y) max(Y(200:400))/1;
+scale(Y)
+
+% d)
+
+W = [];
+S = [];
+
+for i = 0:200,
+    X = [];
+    w = i*(pi/200);
+    for i = 0:N,
+        X(i+1) = x(i,w);
+    end;
+    Y = sistema_desconhecido(X);
+    S(i+1) = scale(Y);
+    S(i+1)
+end;
+
+stem(S)
