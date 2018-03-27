5bc75867c0a85b3f64a17447c93934f5b525b6a3
Rafael Goncalves - 186062 <ra186062@le22-18.grad.fee.unicamp.br>
2018-03-27 14:59:29 -0300

    done plots a, b

--- 2-frequency_domain/efc2.m
+++ 2-frequency_domain/efc2.m
@@ -1,28 +1,40 @@
 x = @(n,w) cos(n*w);
 N = 1000;
+vec = [];
 
-% a)
-w = pi/2;
-for i = 0:N,
-    y = sistema_desconhecido(x(i,w));
-    print(i, y)
-end;
-
-% b)
-w = pi/200;
-for i = 0:N,
-    y = sistema_desconhecido(x(i,w));
-    print(i, y)
-end;
-
-w = pi/20;
-for i = 0:N,
-    y = sistema_desconhecido(x(i,w));
-    print(i, y)
-end;
-
-% c)
-
-% d)
+% % a)
+% w = pi/2;
+% for i = 0:N,
+%     X = [vec; x(i,w)];
+% end;
+% Y = sistema_desconhecido(X)
+% stem(X)
+% hold on
+% stem(Y, 'm')
+% legend('X', 'Y')
+% hold off
 
 
+% % b)
+% w = pi/200;
+% for i = 0:N,
+%     X = [vec; x(i,w)];
+% end;
+% Y = sistema_desconhecido(X)
+% stem(Y)
+% hold on
+% 
+% w = pi/20;
+% for i = 0:N,
+%     X = [vec; x(i,w)];
+% end;
+% Y = sistema_desconhecido(X)
+% stem(Y, 'm')
+% legend('w = pi/200', 'w = pi/20')
+% hold off
+
+% % c)
+% 
+% % d)
+% 
+% 
