d55b330fd38f2e5e79ef90e8d76cdc32dd649680
Rafael Goncalves - 186062 <ra186062@le22-18.grad.fee.unicamp.br>
2018-03-27 15:13:53 -0300

    done plots a, b

--- 2-frequency_domain/efc2.m
+++ 2-frequency_domain/efc2.m
@@ -1,37 +1,39 @@
 x = @(n,w) cos(n*w);
 N = 1000;
-vec = [];
 
-% % a)
-% w = pi/2;
-% for i = 0:N,
-%     X = [vec; x(i,w)];
-% end;
-% Y = sistema_desconhecido(X)
-% stem(X)
-% hold on
-% stem(Y, 'm')
-% legend('X', 'Y')
-% hold off
+% a)
+w = pi/2;
+X = [];
+for i = 0:N,
+    X(i+1) = x(i,w);
+end;
+Y = sistema_desconhecido(X)
+stem(X)
+hold on
+stem(Y, 'm')
+legend('X', 'Y')
+hold off
 
 
-% % b)
-% w = pi/200;
-% for i = 0:N,
-%     X = [vec; x(i,w)];
-% end;
-% Y = sistema_desconhecido(X)
-% stem(Y)
-% hold on
-% 
-% w = pi/20;
-% for i = 0:N,
-%     X = [vec; x(i,w)];
-% end;
-% Y = sistema_desconhecido(X)
-% stem(Y, 'm')
-% legend('w = pi/200', 'w = pi/20')
-% hold off
+% b)
+w = pi/200;
+X = [];
+for i = 0:N,
+    X(i+1) = x(i,w);
+end;
+Y = sistema_desconhecido(X)
+stem(Y)
+hold on
+
+w = pi/20;
+X = [];
+for i = 0:N,
+    X(i+1) = x(i,w);
+end;
+Y = sistema_desconhecido(X)
+stem(Y, 'm')
+legend('w = pi/200', 'w = pi/20')
+hold off
 
 % % c)
 % 
