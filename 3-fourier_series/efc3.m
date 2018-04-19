153d1458376753d288791a665494f053026b3d93
Rafael Goncalves <rafaelgoncalves258@gmail.com>
2018-04-18 21:41:07 -0300

    done all items

--- 3-fourier_series/efc3.m
+++ 3-fourier_series/efc3.m
@@ -5,57 +5,67 @@ w0 = 2*pi/T;
 
 % fourier approximation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-i = 1; % index for figure numbering
+ind = 1; % index for figure numbering
 for n = [1, 10, 20, 50],
-    figure(i);
+    figure(ind);
     X = plot_fourier(n, a, t, w0, 1);
     hold on
     plot(t, t/2, 'r')
     legend('x_n(t)', 'x(t)')
-    i = i+1;
-    Error = sum((t/2 - X).^2)/size(t)(1)
+    ind = ind+1;
+    Error = sum((t/2 - X).^2)/size(t)(2)
     hold off
 end;
 
 % plot |a_k| x w
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 k = -50:50;
-w = (2*pi).*k;
+w = k.*w0;
 A = zeros(size(k));
 for j = k,
     if j != 0,
         A(j+51) = abs(a(j));
     end;
 end;
-figure(i)
-i = i+1;
+figure(ind)
+ind = ind+1;
 stem(w, A)
 xlabel('w')
 ylabel('|a_k|')
 
 % plot abs and angle of frequency response
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-w = -300:0.1:300
 wc = 10;
-h = @(w) 1/(1 - i*(wc/w));
 
 H = [];
-for j = w,
-    H = [H h(j)];
+for b = w,
+    if b != 0,
+        H = [H 1/(1 - (10/b)*i)];
+    else
+        H = [H 0]
+    end;
 end;
 
-figure(i)
-i = i+1;
-plot(w, abs(H))
+figure(ind)
+ind = ind+1;
+stem(w, abs(H))
 xlabel('w')
 ylabel('|H(jw)|')
 
-input('>')
-
-figure(i)
-i = i+1;
-plot(w, angle(H))
+figure(ind)
+ind = ind+1;
+stem(w, angle(H))
 xlabel('w')
 ylabel('arg(H(jw))')
+
+% plot LIT filter in the signal X
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+c = @(k) H(k +51)*a(k); % a_k multiplied by H(jw)
+
+figure(ind);
+Y = plot_fourier(50, c, t, w0, 1)
+xlabel('t')
+ylabel('y(t)')
 
 input('>')
